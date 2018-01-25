463f33f9105d8b24e929c1de07792f1a8a25b387
Perebor <491248+perebor3@users.noreply.github.com>
2018-01-25 14:57:31 +0800

    add 15 days aggregate functions
    Transactions by date
    Block by producer

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 // Express Init
 var express = require("express");
 var app = express();
-var allowCrossDomain = function(req, res, next) {
+var allowCrossDomain = function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.header("Access-Control-Allow-Methods", "GET");
@@ -33,14 +33,17 @@ var data = [
         "baseName": "transactions",
         "tableName": "Transactions"
     },
-    {   "baseName": "messages",
+    {
+        "baseName": "messages",
         "tableName": "Messages"
     }
 ];
 
 
-MongoClient.connect(url, function(err, db) {
-    if (err) { throw err; }
+MongoClient.connect(url, function (err, db) {
+    if (err) {
+        throw err;
+    }
     dbo = db.db(dbName);
     app.listen(3000, "0.0.0.0", function () {
         console.log("Example app listening on port 3000!");
@@ -54,18 +57,18 @@ app.get("/", function (req, res) {
 
 data.forEach(function (item) {
     // Count functions
-    app.get("/" + item.baseName +"/count", function (req, res) {
-        dbo.collection(item.tableName).count(function(err, result) {
+    app.get("/" + item.baseName + "/count", function (req, res) {
+        dbo.collection(item.tableName).count(function (err, result) {
             if (err) throw err;
             console.log(result);
-            res.send({count:result})
+            res.send({count: result})
 
         });
     });
 
     // List functions
     app.get("/" + item.baseName + "/", function (req, res) {
-        dbo.collection(item.tableName).find().sort({ createdAt: -1}).limit(listLimit).toArray(function (err, result) {
+        dbo.collection(item.tableName).find().sort({createdAt: -1}).limit(listLimit).toArray(function (err, result) {
             if (err) throw err;
             console.log(result);
             res.send(result)
@@ -75,7 +78,7 @@ data.forEach(function (item) {
     // Query by _id
     app.get("/" + item.baseName + "/:id", function (req, res) {
         var objId = new ObjectId(req.params.id);
-        dbo.collection(item.tableName).findOne({"_id": objId}, function(err, result) {
+        dbo.collection(item.tableName).findOne({"_id": objId}, function (err, result) {
             if (err) throw err;
             console.log(result);
             res.send(result)
@@ -85,47 +88,86 @@ data.forEach(function (item) {
 
 app.get("/accounts/name/:name", function (req, res) {
     // var o_id = new ObjectId(req.params.name);
-    dbo.collection("Accounts").findOne({"name": req.params.name}, function(err, result) {
+    dbo.collection("Accounts").findOne({"name": req.params.name}, function (err, result) {
         if (err) throw err;
         console.log(result);
         res.send(result)
     });
 });
-
 app.get("/blocks/b/:id", function (req, res) {
-    dbo.collection("Blocks").findOne({"block_id": req.params.id}, function(err, result) {
+    dbo.collection("Blocks").findOne({"block_id": req.params.id}, function (err, result) {
         if (err) throw err;
         console.log(result);
         res.send(result)
     });
 });
 app.get("/transactions/t/:id", function (req, res) {
-    dbo.collection("Transactions").findOne({"transaction_id": req.params.id}, function(err, result) {
+    dbo.collection("Transactions").findOne({"transaction_id": req.params.id}, function (err, result) {
         if (err) throw err;
         console.log(result);
         res.send(result)
     });
 });
-app.get("/transactions/account/:name", function (req, res) {
-    dbo.collection("Transactions").find({"scope": req.params.name}).sort({ createdAt: -1}).limit(listLimit).toArray(function(err, result) {
+app.get("/messages/t/:id", function (req, res) {
+    dbo.collection("Messages").findOne({"transaction_id": req.params.id}, function (err, result) {
         if (err) throw err;
         console.log(result);
         res.send(result)
     });
 });
 
-app.get("/messages/t/:id", function (req, res) {
-    dbo.collection("Messages").findOne({"transaction_id": req.params.id}, function(err, result) {
+app.get("/transactions/account/:name", function (req, res) {
+    dbo.collection("Transactions").find({"scope": req.params.name}).sort({createdAt: -1}).limit(listLimit).toArray(function (err, result) {
         if (err) throw err;
         console.log(result);
         res.send(result)
     });
 });
 app.get("/messages/account/:name", function (req, res) {
-    dbo.collection("Messages").find({$or: [ {"data.from": req.params.name}, {"data.to": req.params.name}]})
-        .sort({ createdAt: -1}).limit(listLimit).toArray(function(err, result) {
+    dbo.collection("Messages").find({$or: [{"data.from": req.params.name}, {"data.to": req.params.name}]})
+        .sort({createdAt: -1}).limit(listLimit).toArray(function (err, result) {
         if (err) throw err;
         console.log(result);
         res.send(result)
     });
+});
+
+app.get("/aggregate/transactions", function (req, res) {
+    var day = new Date();
+    day.setDate(day.getDate() - 15);
+    dbo.collection("Transactions").aggregate([
+        {$match: {"expiration": {"$gte": day}}},
+        {
+            $group: {
+                _id: {$substr: ["$expiration", 0, 10]},
+                count: {$sum: 1}
+            }
+        },
+        {$sort: {_id: -1}}
+    ]).toArray(function (err, result) {
+        if (err) throw err;
+        console.log(result);
+        res.send(result)
+    });
+
+});
+
+app.get("/aggregate/producer", function (req, res) {
+    var day = new Date();
+    day.setDate(day.getDate() - 15);
+    dbo.collection("Blocks").aggregate([
+        {$match: {"timestamp": {"$gte": day}}},
+        {
+            $group: {
+                _id: "$producer_account_id",
+                count: {$sum: 1}
+            }
+        },
+        {$sort: {count: -1}}
+    ]).toArray(function (err, result) {
+        if (err) throw err;
+        console.log(result);
+        res.send(result)
+    });
+
 });
